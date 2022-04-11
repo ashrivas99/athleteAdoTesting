ffda3d44d87d4dba6d235ab2e7fa762d6a4cbef8
adishri99 <61055311+ashrivas99@users.noreply.github.com>
2022-04-11 21:09:09 +0100

    Write API complete

--- backend/services_and_apis/write_athlete_api.py
+++ backend/services_and_apis/write_athlete_api.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, abort
 import database
+from datetime import datetime, timedelta
+import pandas as pd
 from pprint import pprint
-import datetime
 
 write_service = Flask(__name__)
 
@@ -11,30 +12,78 @@ def welcome():
     return "welcome to the write service for athletes"
 
 
+def check_cutoff_time(time_slot_start):
+    time_slot_start_datetime = datetime.fromtimestamp(time_slot_start / 1e3)
+    cutoff_time = datetime.now() + timedelta(days=2)
+
+    cutoff_time_satisfied = True if time_slot_start_datetime > cutoff_time else False
+    return cutoff_time_satisfied
+
+
+def check_timeslot_gt_one_hour(time_slot_start, time_slot_end):
+    time_slot_start_pd = pd.to_datetime(time_slot_start, utc=True, unit="ms")
+    time_slot_end_pd = pd.to_datetime(time_slot_end, utc=True, unit="ms")
+    time_slot_duration = time_slot_end_pd - time_slot_start_pd
+
+    time_slot_duration_in_hours = time_slot_duration.total_seconds() / 3600
+    time_slot_gt_one_hour = True if time_slot_duration_in_hours >= 1 else False
+
+    print("time_slot_duration_in_hours: ", time_slot_duration_in_hours)
+    return time_slot_gt_one_hour
+
+
 @write_service.route("/writeAvailability", methods=["POST"])
 def writeAvailability():
     athlete_availability_info = request.get_json(force=True)
     athlete_email = athlete_availability_info["email"]
     athlete_availability_list = athlete_availability_info["availability"]
-    pprint(athlete_availability_list)
 
     athlete_db_email = database.db.athlete.find_one({"email": athlete_email})
 
     if athlete_db_email is None:
         return jsonify({"message": "athlete not found"}), 404
 
-    for athlete_availability in athlete_availability_list:
+    for idx, athlete_availability in enumerate(athlete_availability_list):
         time_slot_start = athlete_availability["datetime_start"]
         time_slot_end = athlete_availability["datetime_end"]
 
-        # convert to datetime string  in format "YYYY-MM-DD HH:MM:SS" from integer timestamp
-        time_slot_start_str = datetime.datetime.fromtimestamp(
-            time_slot_start / 1e3
-        ).strftime("%Y-%m-%d %H:%M:%S")
+        # validate cutoff time
+        cutoff_satisfied = check_cutoff_time(time_slot_start)
 
-        time_slot_end_str = datetime.datetime.fromtimestamp(
-            time_slot_end / 1e3
-        ).strftime("%Y-%m-%d %H:%M:%S")
+        if not cutoff_satisfied:
+            return (
+                jsonify(
+                    {
+                        "message": "timeslot is past cutoff",
+                        "timeslot_failed": athlete_availability_list[idx],
+                    }
+                ),
+                400,
+            )
+
+        # validate timeslot is greater than one hour
+        time_slot_constraint_satisfied = check_timeslot_gt_one_hour(
+            time_slot_start, time_slot_end
+        )
+
+        if not time_slot_constraint_satisfied:
+            return (
+                jsonify(
+                    {
+                        "message": "timeslot is less than one hour",
+                        "timeslot_failed": athlete_availability_list[idx],
+                    }
+                ),
+                400,
+            )
+
+        time_slot_start_str = datetime.fromtimestamp(time_slot_start / 1e3).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+        time_slot_end_str = datetime.fromtimestamp(time_slot_end / 1e3).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
 
         athlete_availability_object = {
             "time_slot_start": time_slot_start_str,
@@ -69,8 +118,8 @@ def writeAvailability():
                 {"$push": {"availability": athlete_availability_object}},
             )
 
-    return jsonify({"message": "availability added"}), 200
+    return jsonify({"message": "athlete availability added"}), 200
 
 
 if __name__ == "__main__":
-    write_service.run(debug=True)
+    write_service.run(host="0.0.0.0", debug=True)
