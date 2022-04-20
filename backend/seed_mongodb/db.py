6dc0351fae12702ca8edccfe737431cb3cc3f472
adishri99 <61055311+ashrivas99@users.noreply.github.com>
2022-04-20 18:09:42 +0100

    Updating DB string

--- backend/seed_mongodb/db.py
+++ backend/seed_mongodb/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-CONNECTION_STRING = "mongodb+srv://dbuser:password0@cluster0.8ehbg.mongodb.net/main?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://dbuser:password0@cluster1.8ehbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 client = MongoClient(CONNECTION_STRING)
 db = client.main
